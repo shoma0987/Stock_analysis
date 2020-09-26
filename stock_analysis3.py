518fd491c440ca2b87d6ecb3b18d9dfd3e806ae6
shoma0987 <k.shoma1120@gmail.com>
2020-09-26 20:46:06 +0900

    modification

--- stock_analysis3.py
+++ stock_analysis3.py
@@ -3,6 +3,7 @@
 import tkinter
 from tkinter import *
 
+
 with open("stock_225.txt","r",encoding="utf-8", errors="ignore") as f:
     r = f.read()
 N225 = list(r.split())
@@ -15,13 +16,22 @@ for j in range(1,450,2):
    b225.append(N225[j])
 
 window = Tk()
-scrollBar= Scrollbar(window)
+scrollBar = Scrollbar(window, orient=tkinter.VERTICAL)
+#スクロールバーのwidthは一番長い文字列に合わせる
 scrollBar.pack(side=RIGHT, fill=Y)
+window.title("N225")
 
-myList = Listbox(window, yscrollcommand= scrollBar.set)
 
+#スクロールバー
+myList = Listbox(window, height= 17, width= 25, yscrollcommand= scrollBar.set)
 for line in range(225):
-    myList.insert(END, str(line+1)+" : "+a225[line]+" "+b225[line])
+    myList.insert(END, a225[line]+" : "+b225[line])
+
+#ラベルを作成・配置する
+label1 = tkinter.Label(window,
+                       text="東証1部上場会社リスト",
+                       foreground="red")
+label1.pack()
 
 myList.pack()
 scrollBar.config(command = myList.yview)
