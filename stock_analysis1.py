d4d3bf65cd0231f2027a753f7df79acf1cb9a3ba
shoma0987 <k.shoma1120@gmail.com>
2020-09-27 19:07:05 +0900

    add excel sheet

--- stock_analysis1.py
+++ stock_analysis1.py
@@ -2,33 +2,37 @@ from pandas_datareader import data
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-pd.set_option("display.max_columns",10)
-pd.set_option("display.max_rows",1000)
 
-start = "2019-08-01"
-end = "2020-08-01"
-df = data.DataReader("^N225","yahoo",start,end)
+# pd.set_option("display.max_columns",10)
+# pd.set_option("display.max_rows",1000)
+#
+# start = "2019-08-01"
+# end = "2020-08-01"
+# df = data.DataReader("^N225","yahoo",start,end)
+#
+# date = df.index
+# price = df["Adj Close"]
+# span01=5
+# span02=25
+# span03=50
+#
+# df["sma01-5weeks"] = price.rolling(window=span01).mean()
+# df["sma02-25weeks"] = price.rolling(window=span02).mean()
+# df["sma0350weeks"] = price.rolling(window=span03).mean()
+#
+# plt.figure(figsize=(30,10))
+# plt.plot(date,price,label = "Nikkei225")
+#
+# plt.plot(date,df["sma01-5weeks"],label="sma01-5weeks")
+# plt.plot(date,df["sma02-25weeks"],label="sma02-25weeks")
+# plt.plot(date,df["sma0350weeks"],label="sma03-50weeks")
+# plt.xlabel("date",color="black",size=20)
+# plt.ylabel("price",color="black",size=20)
+#
+# plt.legend()
+# plt.show()
 
-date = df.index
-price = df["Adj Close"]
-span01=5
-span02=25
-span03=50
-
-df["sma01-5weeks"] = price.rolling(window=span01).mean()
-df["sma02-25weeks"] = price.rolling(window=span02).mean()
-df["sma0350weeks"] = price.rolling(window=span03).mean()
-
-plt.figure(figsize=(30,10))
-plt.plot(date,price,label = "Nikkei225")
-
-plt.plot(date,df["sma01-5weeks"],label="sma01-5weeks")
-plt.plot(date,df["sma02-25weeks"],label="sma02-25weeks")
-plt.plot(date,df["sma0350weeks"],label="sma03-50weeks")
-plt.xlabel("date",color="black",size=20)
-plt.ylabel("price",color="black",size=20)
-
-plt.legend()
-plt.show()
-
+# pandasによるexcel読み取り
+f1 = pd.read_excel("stock_list.xlsx")
+print(f1)
 
